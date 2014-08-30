573f5c8f74c082c583ea984ca15922cc0a6f5972
Adeel Javed <alwaysadeel@gmail.com>
2014-08-30 09:48:03 +0500

    cleanup

--- Update.sh
+++ Update.sh
@@ -1,7 +1,2 @@
 git pull && ./repo sync -cq -j8
 
-# Missing TI's files
-curl https://raw.githubusercontent.com/CyanogenMod/android_system_netd/cm-11.0/SoftapControllerTI.cpp > system/netd/SoftapControllerTI.cpp
-curl https://raw.githubusercontent.com/CyanogenMod/android_system_netd/cm-11.0/SoftapControllerTI.h > system/netd/SoftapControllerTI.h
-
-
